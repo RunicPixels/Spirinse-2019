01b81419bc24b8cbdacb0b8b6eb6d5801eea78ef
Ronald <ronaldvanegdomdev@gmail.com>
2019-09-20 15:19:49 +0200

    Demo

---
+++ Assets/ResetCharacterPosition.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+using MEC;
+
+public class ResetCharacterPosition : MonoBehaviour
+{
+    Character character;
+
+    private Character.CharacterTypes type;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<Character>();
+        type = character.CharacterType;
+        rb = GetComponent<Rigidbody2D>();
+
+        Timing.RunCoroutine(_UpdateCharacterState());
+    }
+
+    IEnumerator<float> _UpdateCharacterState()
+    {
+        while (gameObject.activeSelf)
+        {
+/*            while (character.CharacterType == type)
+            {
+                yield return 0f;
+            }*/
+
+            type = character.CharacterType;
+
+            switch (type)
+            {
+                case Character.CharacterTypes.AI:
+                    rb.Sleep();
+                    transform.localPosition = Vector3.zero;
+                    break;
+
+                case Character.CharacterTypes.Player:
+                    rb.WakeUp();
+                    break;
+
+                default:
+                    break;
+            }
+
+            yield return 0f;
+        }
+    }
+
+
+}
