ab8ad6b44cd18c339334d6b3a2bc0e8a869563b9
R. van Egdom <superronald@live.nl>
2019-11-19 14:50:47 +0100

    Game Feel Improvements, Sword Chain & Animation Finish

--- Assets/Scripts/System/Camera/CameraManager.cs
+++ Assets/Scripts/System/Camera/CameraManager.cs
@@ -6,9 +6,13 @@ namespace Spirinse.System
 {
     public class CameraManager : MonoBehaviour
     {
+        private static CameraManager instance = null;
+        public static CameraManager Instance => instance;
+
+        public SetCameraDistance cameraDistance;
         private void Awake()
         {
-
+            instance = this;
         }
     }
 }
