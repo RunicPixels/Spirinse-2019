d7b9b4202399f7e5ff60ff8e50b41506c935ceb1
Ronald van Egdom <ronaldvanegdomdev@gmail.com>
2019-11-26 13:16:53 +0100

    added enter

--- Assets/Scripts/System/PostProcessingManager.cs
+++ Assets/Scripts/System/PostProcessingManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PostProcessingManager : MonoBehaviour
 {
     public static PostProcessingManager Instance;
+    
     private void Awake()
     {
         if (Instance == null)
