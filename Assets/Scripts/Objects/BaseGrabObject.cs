80b66a82817557148cd74fda3728b974706cbe0c
Ronald <ronaldvanegdomdev@gmail.com>
2019-11-30 18:15:51 +0100

    Stuff From Friday

--- Assets/Scripts/Objects/BaseGrabObject.cs
+++ Assets/Scripts/Objects/BaseGrabObject.cs
@@ -19,20 +19,26 @@ namespace Spirinse.Objects
 
         public void Hold(Transform newParent)
         {
-            rb.isKinematic = true;
-            transform.parent = newParent;
-            transform.localPosition = new Vector3(0, -1, 0);
+            var offset = 0f;
             foreach(Collider2D col in colliders)
             {
                 col.enabled = false;
+                if (!col.isTrigger) offset = col.bounds.size.y;
+
             }
+            
+            rb.isKinematic = true;
+            transform.parent = newParent;
+
+            transform.localPosition = new Vector3(0, offset , 0);
+
         }
 
         public void Release(Vector3 velocity)
         {
             rb.isKinematic = false;
             // Slight Delay until collisions return;
-            Invoke("ReturnCollision", 0.1f);
+            Invoke(nameof(ReturnCollision), 0.1f);
             transform.parent = null;
             rb.velocity = velocity;
         }
