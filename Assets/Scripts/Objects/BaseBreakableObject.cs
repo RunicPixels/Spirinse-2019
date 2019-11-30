ba976ac751b3db51dd5330643a9e5d4f56be226c
R. van Egdom <superronald@live.nl>
2019-11-30 18:36:27 +0100

    Improved Collapse (need to fix enemy rock hit)

--- Assets/Scripts/Objects/BaseBreakableObject.cs
+++ Assets/Scripts/Objects/BaseBreakableObject.cs
@@ -3,20 +3,38 @@ using UnityEngine;
 
 namespace Spirinse.Objects
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class BaseBreakableObject : MonoBehaviour, IBreakable
     {
-        private void OnCollisionEnter2D(Collision2D col)
+        private Rigidbody2D rb;
+        private void Awake()
         {
-            if (col.transform.GetComponent<BaseGrabObject>())
+            foreach (Rigidbody2D rb in GetComponentsInChildren<Rigidbody2D>())
+            {
+                rb.isKinematic = true;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.GetComponent<BaseGrabObject>() && col.tag == "Throwable")
             {
                 Break();
             }
-            
+
         }
 
         public void Break()
         {
-            Destroy(gameObject);
+            foreach (Rigidbody2D rb in GetComponentsInChildren<Rigidbody2D>())
+            {
+                rb.isKinematic = false;
+            }
+            //Invoke("Destroy", 1f);
+        }
+        public void Destroy()
+        {
+            //Destroy(gameObject);
         }
     }
 }
