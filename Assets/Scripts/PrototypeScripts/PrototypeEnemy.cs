6f809357e4542aa887d0581f1812bb4c99fa9be9
R. van Egdom <superronald@live.nl>
2019-12-18 00:14:45 +0100

    made velocity prediction a bit more gentle.

--- Assets/Scripts/PrototypeScripts/PrototypeEnemy.cs
+++ Assets/Scripts/PrototypeScripts/PrototypeEnemy.cs
@@ -136,7 +136,7 @@ using Spirinse.Objects;
                 break;
             case EnemyState.Moving:
                 speed = idleSpeed;
-                direction = (target.position + ((Vector3)target.GetComponent<Rigidbody2D>().velocity * 0.2f) - transform.position).normalized;
+                direction = (target.position + ((Vector3)target.GetComponent<Rigidbody2D>().velocity * 0.1f) - transform.position).normalized;
                 break;
         }
 
