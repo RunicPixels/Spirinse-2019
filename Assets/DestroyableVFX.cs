a812b55a4813430848845a259103aff8058ba146
Cheezegami <ronaldvanegdomdev@gmail.com>
2020-01-09 19:58:53 +0100

    staged Changes

---
+++ Assets/DestroyableVFX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class DestroyableVFX : MonoBehaviour
+{
+    public VisualEffect vfx;
+
+    public Color effectColor;
+
+    public float height;
+    public float width;
+
+    private float usedHeight;
+    private float usedWidth;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        usedHeight = height * transform.lossyScale.y;
+        usedWidth = width * transform.lossyScale.x;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Color col = new Color(effectColor.r, effectColor.g, effectColor.b, 0.1f);
+        Gizmos.color = col;
+        Vector3 box = new Vector3(width,height);
+        Gizmos.DrawCube(transform.position, box);
+    }
+}
--- Assets/Scripts/PrototypeScripts/Bullet.cs
+++ Assets/Scripts/PrototypeScripts/Bullet.cs
@@ -35,7 +35,7 @@ public class Bullet : MonoBehaviour, IAttack
         
         Vector2 v = rb.velocity;
         var angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     private void FixedUpdate()
