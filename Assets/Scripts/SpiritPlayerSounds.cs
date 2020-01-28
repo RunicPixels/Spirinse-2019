0b26d62d75ba63ea42d61d49bab0829074be8d8f
Ronald van Egdom <ronaldvanegdomdev@gmail.com>
2020-01-28 12:39:45 +0100

    fix? maybe, git gui is being dumb

---
+++ Assets/Scripts/SpiritPlayerSounds.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using FMODUnity;
+using Spirinse.Player;
+using Spirinse.System;
+using Spirinse.System.Player;
+
+namespace Spirinse.Audio
+{
+    public class SpiritPlayerSounds : MonoBehaviour
+    {
+        [SerializeField] protected AbilitySound[] AbilitySounds;
+        [Serializable] public class AbilitySound
+        {
+            private Rigidbody2D cachedRB;
+            private Player.Player player;
+            public BaseAbility ability;
+            [FMODUnity.EventRef] public string AbilityUseEvent;
+
+            private FMOD.Studio.EventInstance _eventInstance;
+
+            public void Update()
+            {
+                _eventInstance.set3DAttributes(
+                    FMODUnity.RuntimeUtils.To3DAttributes(
+                        player.defender.transform.gameObject,
+                        cachedRB));
+            }
+            
+            public void Begin()
+            {
+                _eventInstance = FMODUnity.RuntimeManager.CreateInstance(AbilityUseEvent);
+                ability.OnAbilityUse += PlaySound;
+                cachedRB = Player.Player.Instance.controls.GetRB();
+                player = Player.Player.Instance;
+
+            }
+            
+            public void PlaySound()
+            {
+
+                
+                _eventInstance.start();
+                //FMODUnity.RuntimeManager.PlayOneShot(AbilityUseEvent,Player.Player.Instance.defender.transform.position);
+            }
+        }
+        
+        private void Start()
+        {
+            for (int i = 0; i < AbilitySounds.Length; i++)
+            {
+                AbilitySounds[i].Begin();
+            }
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < AbilitySounds.Length; i++)
+            {
+                AbilitySounds[i].Update();
+            }
+        }
+    }
+}
