68f7753ab69b9b154bfe05e7e0673613ee29939a
Cheezegami <superronald@live.nl>
2020-03-10 15:54:05 +0100

    working

--- Assets/OverrideCamera.cs
+++ Assets/OverrideCamera.cs
@@ -1,10 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Spirinse.System;
 using UnityEngine;
 
 public class OverrideCamera : MonoBehaviour
 {
+    public enum CameraMode { FollowPlayer, StaticTarget, StaticLookAtPlayer, Inbetween }
+
+    private CameraMode _mode;
+    public CameraMode Mode
+    {
+        get => _mode;
+        set
+        {
+            _mode = value;
+            SwapEnemMode();
+        }
+        
+
+    }
     private CameraManager _cameraManager;
 
     public float distance = 50;
@@ -30,6 +45,19 @@ public class OverrideCamera : MonoBehaviour
         if (other.CompareTag("Defender"))
         {
             _cameraManager.cameraDistance.SetNewCameraDistance(oldDistance);
+        }
+    }
+
+    public void SwapEnemMode()
+    {
+        switch (_mode)
+        {
+            case CameraMode.FollowPlayer :
+                distance = 30;
+                break;
+            default :
+                distance = 20;
+                break;
         }
     }
 }
