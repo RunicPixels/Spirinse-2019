53b2a932de079b8381e62dc6301110e60cd8aa66
Cheezegami <superronald@live.nl>
2020-03-10 13:35:13 +0100

    Camera Panning Shit made

--- Assets/Scripts/Effects/SetCameraDistance.cs
+++ Assets/Scripts/Effects/SetCameraDistance.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 
 public class SetCameraDistance : MonoBehaviour
 {
-    private float baseDistance = 4f;
     public float cameraDistance = 25f;
 
+    private float baseDistance;
     public float velocityModifier = 0.3f;
     private float velocityDistance = 0;
+    private float newDistance = 0;
 
+    private float swapTime;
+
+    private void Awake()
+    {
+        newDistance = cameraDistance;
+        baseDistance = cameraDistance;
+    }
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         var position = transform.position;
         var distance = cameraDistance + velocityDistance;
-        position = new Vector3(position.x,position.y,-distance);
+        distance = Mathf.Lerp(distance, newDistance, swapTime);
+        if (swapTime < 1f)
+        {
+            swapTime += Time.deltaTime;
+            if (swapTime > 1f) swapTime = 1f;
+        }
+        position = new Vector3(position.x, distance / 5.33f, -distance);
         transform.position = position;
     }
+
     public void SetVelocityDistance(float velocity)
     {
         velocityDistance = velocity * velocityModifier;
     }
+
+    public void SetNewCameraDistance(float distance)
+    {
+        swapTime = 0;
+        newDistance = distance;
+    }
+
+    public float GetBaseDistance()
+    {
+        return baseDistance;
+    }
+
 }
---
+++ Assets/SetCameraPosition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Spirinse.System;
+using UnityEngine;
+
+public class SetCameraPosition : MonoBehaviour
+{
+    private CameraManager _cameraManager;
+
+    public float distance = 50;
+    private float oldDistance;
+
+    private void Start()
+    {
+        _cameraManager = CameraManager.Instance;
+        oldDistance = _cameraManager.cameraDistance.GetBaseDistance();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.CompareTag("Defender"))
+        {
+            _cameraManager.cameraDistance.SetNewCameraDistance(distance);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Defender"))
+        {
+            _cameraManager.cameraDistance.SetNewCameraDistance(oldDistance);
+        }
+    }
+}
