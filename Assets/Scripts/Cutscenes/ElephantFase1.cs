c553dcab522d47af21c30c0a5b9e52e4e4da08c7
DaphneZwarts <47414528+DaphneZwarts@users.noreply.github.com>
2020-05-01 11:29:33 +0200

    Entire Boss Level (IMPORTANT)
    Please don't create a merge conflict... please...

---
+++ Assets/Scripts/Cutscenes/ElephantFase1.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElephantFase1 : MonoBehaviour
+{
+
+    public Animator ElephantAnim;
+    public Animator TreeAnim;
+
+    public GameObject Elephant1;
+    public GameObject Elephant2;
+
+    public int Health = 30;
+    public int Damage = 5;
+
+    private void Start()
+    {
+        ElephantAnim.SetBool("Hit", false);
+        ElephantAnim.SetBool("SwordHit", false);
+        ElephantAnim.SetBool("FallDownHill", false);
+        Elephant1.SetActive(true);
+        Elephant2.SetActive(false);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D elephant)
+    {
+        if (elephant.CompareTag("Throwable") && ElephantAnim.GetCurrentAnimatorStateInfo(0).IsName("PranceIdle"))
+        {
+            ElephantAnim.SetBool("Prance", false);
+            ElephantAnim.SetBool("Hit", true);
+            StartCoroutine(DownForDamage()); 
+        }
+
+        else if (elephant.CompareTag("Selected") && ElephantAnim.GetCurrentAnimatorStateInfo(0).IsName("StandTired"))
+        {
+            ElephantAnim.SetBool("SwordHit", true);
+            Health -= Damage;
+            if (Health <= 0)
+            {
+                Health = 0;
+                TreeAnim.SetBool("CutOff", true);
+                ElephantAnim.SetBool("FallDownHill", true);
+                StartCoroutine(ElephantSwitch());
+            }
+        }
+
+        else
+        {
+            ElephantAnim.SetBool("Hit", false);
+            ElephantAnim.SetBool("SwordHit", false);
+            ElephantAnim.SetBool("Prance", false);
+        }
+    }
+
+    private IEnumerator ElephantSwitch()
+    {
+        yield return new WaitForSeconds(2);
+        Elephant1.SetActive(false);
+        Elephant2.SetActive(true);   
+    }
+
+    private IEnumerator DownForDamage()
+    {
+        yield return new WaitForSeconds(2);
+        ElephantAnim.SetBool("Hit", false);
+        ElephantAnim.SetBool("SwordHit", false);
+        yield return new WaitForSeconds(10);
+        ElephantAnim.SetBool("Prance", true);
+    }
+}
---
+++ Assets/Scripts/Cutscenes/ElephantFase2.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElephantFase2 : MonoBehaviour
+{
+    public float speed;
+
+    public bool moveRight;
+
+    public bool noMovement;
+
+    public Animator elephantController;
+
+
+    private void Start()
+    {
+        noMovement = true;
+        elephantController.SetBool("GetUp", false);
+    }
+
+
+    void Update()
+    {
+        if (noMovement)
+        {
+            transform.Translate(0 * Time.deltaTime * speed, 0, 0);
+            elephantController.SetBool("Walking", false);
+        }
+
+        else if (moveRight)
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            GetComponent<Transform>().eulerAngles = new Vector3(0, 180, 0);
+            elephantController.SetBool("Walking", true);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            GetComponent<Transform>().eulerAngles = new Vector3(0, 0, 0);
+            elephantController.SetBool("Walking", true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.CompareTag("Defender"))
+        {
+            elephantController.SetBool("GetUp", true);
+            StartCoroutine(GetUpToFight());
+        }
+
+        if (trig.gameObject.CompareTag("Turn")){
+
+            if (moveRight)
+            { moveRight = false; }
+
+            else
+            { moveRight = true; }
+        }
+    }
+
+    private IEnumerator GetUpToFight()
+    {
+        {
+            yield return new WaitForSeconds(4);
+            noMovement = false;
+        }
+    }
+}
